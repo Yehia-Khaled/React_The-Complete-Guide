81b47e5e4a55550ecc869748dacb1310dad7bfa9
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-11-21 21:24:39 +0200

    add components directory to src folder

---
+++ 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Expenseitem extends React.Component {
+    render() {
+        return <div>Hi Yehia</div>;
+    }
+}
+
+export default Expenseitem;
