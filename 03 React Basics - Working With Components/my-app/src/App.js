ae244940f11e6cbbc3908b846b3922012216efc4
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-11-30 22:03:12 +0200

    Lecture 013 Passing Data via props code and notes

--- 03 React Basics - Working With Components/my-app/src/App.js
+++ 03 React Basics - Working With Components/my-app/src/App.js
@@ -3,14 +3,38 @@ import './App.css';
 import Expenseitem from './Components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
       </header>
       <body>
-        <Expenseitem></Expenseitem>
+        <Expenseitem expenses={expenses[0]}></Expenseitem>
+        <Expenseitem expenses={expenses[1]}></Expenseitem>
+        <Expenseitem expenses={expenses[2]}></Expenseitem>
+        <Expenseitem expenses={expenses[3]}></Expenseitem>
+
       </body>
     </div>
   );
--- 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
+++ 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
@@ -3,15 +3,13 @@ import './ExpenseItem.css'
 
 class Expenseitem extends React.Component {
     render() {
-        const expenseDate = new Date(2021, 11, 25);
-        const expenseItem = 'Car insurance';
-        const expenseAmount = "294.66"
+        const { expenses } = this.props
         return (
             <div className='expense-item'>
-                <div>Date : {expenseDate.toISOString()}</div>
+                <div>Date : {expenses.date.toISOString()}</div>
                 <div className='expense-item__description'>
-                    <h2>Title : {expenseItem}</h2>
-                    <div className='expense-item__price'>Amount : ${expenseAmount} </div>
+                    <h2>Title : {expenses.title}</h2>
+                    <div className='expense-item__price'>Amount : ${expenses.amount} </div>
                 </div>
             </div>
         );
