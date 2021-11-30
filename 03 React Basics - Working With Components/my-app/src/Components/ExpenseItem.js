8b212cd58cc2d4ae9e9586b144a4328ddb2ceda5
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-11-30 22:11:15 +0200

    Lecture 014 Adding _normal_ JavaScript Logic to Components code and notes

--- 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
+++ 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
@@ -4,9 +4,17 @@ import './ExpenseItem.css'
 class Expenseitem extends React.Component {
     render() {
         const { expenses } = this.props
+        const month=expenses.date.toLocaleString('en-US',{ month : 'long'});
+        const day=expenses.date.toLocaleString('en-US',{ day : '2-digit'});
+        const year =expenses.date.getFullYear();
+
         return (
             <div className='expense-item'>
-                <div>Date : {expenses.date.toISOString()}</div>
+                <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+                </div>
                 <div className='expense-item__description'>
                     <h2>Title : {expenses.title}</h2>
                     <div className='expense-item__price'>Amount : ${expenses.amount} </div>
