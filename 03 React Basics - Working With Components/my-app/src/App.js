f47c2b96852c74ce8cf90f57f957bd86bd2365b2
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-12-07 20:20:36 +0200

    Lecture 016 The Concept of _Composition_ (_children props_) code and notes

--- 03 React Basics - Working With Components/my-app/src/App.js
+++ 03 React Basics - Working With Components/my-app/src/App.js
@@ -1,41 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenseitem from './Components/ExpenseItem';
+import Expenses from './Components/Expenses'
+
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <body>
-        <Expenseitem expenses={expenses[0]}/>
-        <Expenseitem expenses={expenses[1]}/>
-        <Expenseitem expenses={expenses[2]}/>
-        <Expenseitem expenses={expenses[3]}/>
+          <Expenses/>
 
-      </body>
     </div>
   );
 }
--- 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
+++ 03 React Basics - Working With Components/my-app/src/Components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './ExpenseItem.css';
 import ExpenseItemDate from "./ExpenseItemDate";
+import Card from "./Card";
 
 class Expenseitem extends React.Component {
     render() {
         const { expenses } = this.props
         return (
-            <div className='expense-item'>
+            <Card className='expense-item'>
                 <ExpenseItemDate expenseDate={expenses.date}/>
                 <div className='expense-item__description'>
                     <h2>{expenses.title}</h2>
                     <div className='expense-item__price'>${expenses.amount} </div>
                 </div>
-            </div>
+            </Card>
         );
     }
 }
