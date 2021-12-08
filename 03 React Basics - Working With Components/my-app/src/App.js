0a038ff1757863a25453897f9a4cfa7120595ed5
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-12-08 19:31:01 +0200

    Lecture 018 A Closer Look At JSX code and notes

--- 03 React Basics - Working With Components/my-app/src/App.js
+++ 03 React Basics - Working With Components/my-app/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Expenseitem from './Components/ExpenseItem';
@@ -5,16 +6,44 @@ import Expenses from './Components/Expenses'
 
 
 function App() {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
 
-  return (
+    //Without using Jsx
+    return React.createElement('div',{},
+        React.createElement('h2',{},'Hi Yehia'),
+        React.createElement(Expenses,{expenses:expenses})
+        );
+
+    //Using JSX
+ /* return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-          <Expenses/>
+          <Expenses expenses={expenses}/>
 
     </div>
-  );
+  );*/
 }
 
 export default App;
