a913135bcafdd8eee15ac83dc11fe98cabdc36c2
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-12-12 00:03:22 +0200

    Lecture 002 Listening to Events & Working with Event Handlers code and notes

--- Project of Course/src/Components/Expenses/ExpenseItem.js
+++ Project of Course/src/Components/Expenses/ExpenseItem.js
@@ -6,6 +6,10 @@ import Card from "../UI/Card";
 class Expenseitem extends React.Component {
     render() {
         const { expenses } = this.props
+
+        const clickHandler=()=>{
+            console.log('Clicked!!')
+        }
         return (
             <Card className='expense-item'>
                 <ExpenseItemDate expenseDate={expenses.date}/>
@@ -13,6 +17,7 @@ class Expenseitem extends React.Component {
                     <h2>{expenses.title}</h2>
                     <div className='expense-item__price'>${expenses.amount} </div>
                 </div>
+                <button onClick={clickHandler}> Change Title</button>
             </Card>
         );
     }
