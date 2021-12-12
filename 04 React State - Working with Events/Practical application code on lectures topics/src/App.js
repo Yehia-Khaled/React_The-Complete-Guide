19f81c7e8747c8089eb53cb15d34d3ac19683f32
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-12-12 22:25:44 +0200

    learn on section 04 React State - Working with Events

---
+++ 04 React State - Working with Events/Practical application code on lectures topics/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import Expenses from './Components/Expenses/Expenses'
+
+
+function App() {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+    /*  //Without using Jsx
+      return React.createElement('div',{},
+          React.createElement('h2',{},'Hi Yehia'),
+          React.createElement(Expenses,{expenses:expenses})
+          );*/
+
+    //Using JSX
+    return (
+        <div className="App">
+            <Expenses expenses={expenses}/>
+        </div>
+    );
+}
+
+export default App;
