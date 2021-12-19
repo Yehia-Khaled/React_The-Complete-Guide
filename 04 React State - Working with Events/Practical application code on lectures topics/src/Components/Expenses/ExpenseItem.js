bc53a91d173c09081bc0e28cb8f8d191231fe149
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-12-19 16:50:13 +0200

    Lecture 016 Controlled vs Uncontrolled Components & Stateless vs Stateful Components code and notes

--- 04 React State - Working with Events/Practical application code on lectures topics/src/Components/Expenses/ExpenseItem.js
+++ 04 React State - Working with Events/Practical application code on lectures topics/src/Components/Expenses/ExpenseItem.js
@@ -2,31 +2,31 @@ import React from 'react';
 import './ExpenseItem.css';
 import ExpenseItemDate from "./ExpenseItemDate";
 import Card from "../UI/Card";
-import {useState} from "react";
+// import {useState} from "react";
 
 //using function component
 const Expenseitem = (props) => {
     const {expenses} = props
-    const [title, setTitle] = useState(expenses.title); //first element is the current state value, and the second is  a function for updating that.
 
-    console.log('ExpenseItem evaluated by react')
+    // const [title, setTitle] = useState(expenses.title); //first element is the current state value, and the second is  a function for updating that.
+    // console.log('ExpenseItem evaluated by react')
+    /*    // let title = expenses.title;
+        const clickHandler = () => {
+            // console.log('Clicked!!')
+            setTitle('Update!!') // we use this to execute function again after click or call this function or event happen
 
-    // let title = expenses.title;
-    const clickHandler = () => {
-        // console.log('Clicked!!')
-        setTitle('Update!!') // we use this to execute function again after click or call this function or event happen
+            // title = 'Update!!'
+            console.log(title);
+        }*/
 
-        // title = 'Update!!'
-        console.log(title);
-    }
     return (
         <Card className='expense-item'>
             <ExpenseItemDate expenseDate={expenses.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{expenses.title}</h2>
                 <div className='expense-item__price'>${expenses.amount} </div>
             </div>
-            <button onClick={clickHandler}> Change Title</button>
+            {/*<button onClick={clickHandler}> Change Title</button>*/}
         </Card>
     );
 }
