c81543e4adf1430cef76dd319607e585d4defa82
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2022-01-17 00:07:30 +0200

    Lecture 008 Dynamic Styles with CSS Modules code and notes

--- 06 Styling React Components/ToDo React Project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 06 Styling React Components/ToDo React Project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -25,7 +25,8 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`form-control ${!isValid ? 'invalid ' : ''}`}>
+            <div
+                className={`${styles['form-control']} ${!isValid && styles.invalid}`}>{/*`form-control ${!isValid ? 'invalid ' : ''}`*/}
                 <label>Course Goal</label> {/*inline style: {color: !isValid ? 'red' : 'black'}*/}
                 <input
                     type="text"
