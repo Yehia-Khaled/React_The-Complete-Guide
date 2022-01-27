6cff0bdbcc036203e4a00317486999053746543b
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2022-01-27 22:52:32 +0200

    Lecture 005 Managing the User Input State code and notes

--- 08 Time to Practice_ A Complete Practice Project/add-user-app/src/components/Users/AddUser.js
+++ 08 Time to Practice_ A Complete Practice Project/add-user-app/src/components/Users/AddUser.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, {useState} from 'react';
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import classes from './AddUser.module.css'
 
-const addUser = (props) => {
+const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState(''); /*Array destructing*/
+    const [enteredAge, setEnteredAge] = useState(''); /*Array destructing*/
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge)
+    }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username" placeholder="Please Enter your Name">UserName</label>
-                <input id='username' type="text"/>
+                <input id='username' type="text" onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id='age' type="number"/>
+                <input id='age' type="number" onChange={ageChangeHandler}/>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
     );
 }
 
-export default addUser;
+export default AddUser;
